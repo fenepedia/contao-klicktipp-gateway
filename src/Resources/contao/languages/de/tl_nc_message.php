ad7ed1bacb46d70b862adc33f15651b7b7e2c7ad
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2020-06-08 19:26:38 +0100

    add missing translations

--- src/Resources/contao/languages/de/tl_nc_message.php
+++ src/Resources/contao/languages/de/tl_nc_message.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
+$GLOBALS['TL_LANG']['tl_nc_message']['klicktipp_legend'] = 'Klick-Tipp API Request Einstellungen';
 $GLOBALS['TL_LANG']['tl_nc_message']['kt_action'] = ['Aktion', 'Aktion/Endpunkt'];
 $GLOBALS['TL_LANG']['tl_nc_message']['kt_email'] = ['E-Mail Adresse', 'E-mail Adresse mit der ein Subscriber identifiziert wird.'];
 $GLOBALS['TL_LANG']['tl_nc_message']['kt_parameters'] = ['Parameter', 'Parameter für den API Request.'];
--- src/Resources/contao/languages/en/tl_nc_message.php
+++ src/Resources/contao/languages/en/tl_nc_message.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
+$GLOBALS['TL_LANG']['tl_nc_message']['klicktipp_legend'] = 'Klick-Tipp API request settings';
 $GLOBALS['TL_LANG']['tl_nc_message']['kt_action'] = ['Action', 'Action/Endpoint'];
 $GLOBALS['TL_LANG']['tl_nc_message']['kt_email'] = ['E-mail address', 'E-mail address with which to identify a subscriber.'];
 $GLOBALS['TL_LANG']['tl_nc_message']['kt_parameters'] = ['Parameters', 'Parameters for the API request.'];
